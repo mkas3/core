b03247c117baca313abc85ee1c59024787a80e36
mkas3 <madTwo.19@yandex.ru>
2024-08-06 12:16:06 +0300

    fix: remove react config from index

--- core/eslint/index.js
+++ core/eslint/index.js
@@ -1,5 +1,4 @@
 const eslintNext = require('./.eslintrc.next.js');
-const eslintReact = require('./.eslintrc.react.js');
 const eslintNode = require('./.eslintrc.node.js');
 
-module.exports = { eslint: { next: eslintNext, react: eslintReact, node: eslintNode } };
+module.exports = { eslint: { next: eslintNext, node: eslintNode } };
