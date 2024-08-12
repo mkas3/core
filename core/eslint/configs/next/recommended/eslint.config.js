88138391c13e1bc4992a543ce64f8cf1860d7e37
mkas3 <madTwo.19@yandex.ru>
2024-08-12 21:16:17 +0300

    fix: fix config

--- core/eslint/configs/next/recommended/eslint.config.js
+++ core/eslint/configs/next/recommended/eslint.config.js
@@ -1,5 +1,38 @@
 const { antfu } = require('@antfu/eslint-config');
 
 module.exports = antfu({
-  react: true
+  javascript: {
+    overrides: {
+      'antfu/top-level-function': 'off'
+    }
+  },
+  typescript: {
+    overrides: {
+      'ts/consistent-type-definitions': ['error', 'type']
+    }
+  },
+  react: true,
+  stylistic: {
+    quotes: 'single',
+    jsx: true,
+    indent: 2,
+    semi: true,
+    overrides: {
+      'style/comma-dangle': ['error', 'never'],
+      'style/member-delimiter-style': [
+        {
+          multiline: {
+            delimiter: 'semi',
+            requireLast: true
+          },
+          singleline: {
+            delimiter: 'semi',
+            requireLast: false
+          },
+          multilineDetection: 'brackets'
+        }
+      ],
+      'style/arrow-parens': ['always']
+    }
+  }
 });
